0cd2c50c3a401682f3cb36fafb19b16e4e379d31
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2025-01-15 12:55:41 +0530

    New commit

--- Sem3.c
+++ Sem3.c
@@ -1,32 +1,41 @@
-#include<stdio.h>
-void main(){
-//	int i=1,j,n,s=0,sum=0;
-//	printf("Enter n");
-//	scanf("%d",&n);
-//	for(j=2;j<=n;j++){
-//		while(j>=i){
-//			if(j%i==0)
-//				s+=i;
-//			i++;
-//		}
-//		if(s==(j+1)){
-//			printf("%d is prime",j);
-//			printf("\n");
-//			sum+=j;
-//		}
-//		i=1;
-//		s=0;
-//	}
-//	printf("\nSum is %d",sum);
 
-//	int n=650;
-//	//printf("Enter an int within range (1-1000)");
-//	//scanf("%d",&n);
-//	if (n%1000==0)
-//	if(n%500)
-//	if(n%100)
-//	if(n%50)
-//	if(n%10)
-//	if(n%5)
-	
+//void main(){
+////	int i=1,j,n,s=0,sum=0;
+////	printf("Enter n");
+////	scanf("%d",&n);
+////	for(j=2;j<=n;j++){
+////		while(j>=i){
+////			if(j%i==0)
+////				s+=i;
+////			i++;
+////		}
+////		if(s==(j+1)){
+////			printf("%d is prime",j);
+////			printf("\n");
+////			sum+=j;
+////		}
+////		i=1;
+////		s=0;
+////	}
+////	printf("\nSum is %d",sum);
+//
+////	int n=650;
+////	//printf("Enter an int within range (1-1000)");
+////	//scanf("%d",&n);
+////	if (n%1000==0)
+////	if(n%500)
+////	if(n%100)
+////	if(n%50)
+////	if(n%10)
+////	if(n%5)
+//
+//	
+//}
+#include<stdio.h>;
+#define square(x) x*x
+int main(){
+int x;
+x = 36/square(6);
+printf("%d", x);
+return 0;
 }
